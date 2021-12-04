df38effaa43c94bbde3467af830d8844a57098ec
Emmanuel-EEzekiel <emamex44@gmail.com>
2021-12-04 20:32:30 +0100

    add file

---
+++ index.js
@@ -0,0 +1,21 @@
+
+function change(){
+	var basic = "$54";
+	var standard = "$50.4";
+	var business = "$46.8";
+
+	document.getElementById("1").innerHTML = basic;
+	document.getElementById("2").innerHTML = standard;
+	document.getElementById("3").innerHTML = business;
+}
+
+function onClick() {
+    var basic = "$4.5";
+	var standard = "$4.2";
+	var business = "$3.9";
+
+	document.getElementById("1").innerHTML = basic;
+	document.getElementById("2").innerHTML = standard;
+	document.getElementById("3").innerHTML = business;
+}
+
